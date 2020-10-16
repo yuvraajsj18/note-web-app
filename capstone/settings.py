e6e2d4e244e26ce893e0cddf6921129b6f139fbf
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2020-10-16 21:51:17 +0530

    added heorku postgres db

--- capstone/settings.py
+++ capstone/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 # Use custom User model
 AUTH_USER_MODEL = "note.User"
 
